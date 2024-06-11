227c276378889a295fb0383140ca683de54caacd
mwdev98 <mdev80734@gmail.com>
2024-06-11 12:03:19 -0400

    updated api link

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
+
 var activityContainer=document.getElementById("activity-container");
 var button=document.getElementById("button");
 
 
 button.addEventListener('click',()=>{
-  fetch("http://www.boredapi.com/api/activity/").then((res)=>{
+  fetch("http://bored.api.lewagon.com/api/activity/").then((res)=>{
    //console.log(data);
    return res.json();
    }).then((data)=>{
 
-   for(let i=0; i<data.activity.length; i++){
+   for(let i=0; i< data.activity.length; i++){
    activityContainer.innerHTML='';
    var act=document.createElement("h4");
    act.textContent=data.activity;
@@ -17,3 +18,13 @@ button.addEventListener('click',()=>{
 })
 
 })
+
+//http://www.boredapi.com/api/activity/
+
+
+
+
+
+
+
+
